c579d7053be41684c184ae32aebd7f629b8c56d2
Jessica Brumfield <jbrumfield1999@gmail.com>
2018-10-10 18:22:18 +0000

    automatically backed up by learn

--- other_file.js
+++ other_file.js
@@ -1,8 +1,8 @@
 var index = require("./index.js")
 
-//function a (){console.log(index.name)
+function a (){console.log(index.name)
 
-//}
-//a()
-function b () {console.log('Hello')}
-b()
+}
+a()
+// function b () {console.log('Hello')}
+// b()
